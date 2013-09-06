627b48ef5d6ec45f5ce6779bdf33872ce8d3e0f7
Jeffrey Pierce <jeffrey.piece@dealer.com>
2013-09-06 11:34:30 -0400

    settings fro in.c performance no. 2 at the burlington arthop

---
+++ conf/ddc.js
@@ -0,0 +1,14 @@
+module.exports = [
+	{
+		'device': '/dev/tty.usbmodem1d1131',
+		'name': 'ard1'
+	},
+	{
+		'device': '/dev/tty.usbmodem1d1121',
+		'name': 'ard2'
+	},
+	{
+		'device': '/dev/tty.usbmodem1d11111',
+		'name': 'ard3'
+	}
+];
